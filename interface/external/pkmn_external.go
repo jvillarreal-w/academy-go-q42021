ceb6d9a319cea5c1f02f3d1771f0643eb6c88a55
Jaime Villarreal <jaime.villarreal@wizeline.com>
2021-11-24 16:36:06 -0600

    Added external Pokemon details

--- interface/external/pkmn_external.go
+++ interface/external/pkmn_external.go
@@ -27,7 +27,7 @@ func NewPokemonExternal() PokemonExternal {
 }
 
 func (pe *pokemonExternal) GetExternalPokemon(p []*model.Pokemon, c context.Context) ([]*model.Pokemon, error) {
-	request, err := http.Get(fmt.Sprintf("%v?limit=151", url))
+	request, err := http.Get(fmt.Sprintf("%v?limit=12", url))
 
 	if err != nil {
 		u.ErrorLogger.Println("External resource is not reachable")
@@ -37,7 +37,7 @@ func (pe *pokemonExternal) GetExternalPokemon(p []*model.Pokemon, c context.Cont
 	body, err := ioutil.ReadAll(request.Body)
 
 	if err != nil {
-		u.ErrorLogger.Println("Response could not be read")
+		u.ErrorLogger.Println("Response body could not be read")
 		return nil, err
 	}
 
@@ -50,17 +50,75 @@ func (pe *pokemonExternal) GetExternalPokemon(p []*model.Pokemon, c context.Cont
 	for i, val := range response.Results {
 		id := uint64(i + 1)
 
-		pkmn := &model.Pokemon{
-			ID:         id,
-			Name:       val.Name,
-			Generation: getGeneration(id),
+		details_request, err := http.Get(fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%v/", id))
+
+		if err != nil {
+			u.ErrorLogger.Printf("External details are not reachable: %s", err)
+			return nil, err
 		}
 
-		u.GeneralLogger.Printf("%v", pkmn.ID)
+		details_body, err := ioutil.ReadAll(details_request.Body)
 
-		p = append(p, pkmn)
+		if err != nil {
+			u.ErrorLogger.Println("Details body could not be read")
+			return nil, err
+		}
+
+		details_response := external.PokemonExternalDetails{}
+
+		if err := json.Unmarshal(details_body, &details_response); err != nil {
+			u.ErrorLogger.Printf("Details could not be unmarshalled: %v", err)
+			return nil, err
+		}
+
+		if len(details_response.Types) < 2 {
+			pkmn := &model.Pokemon{
+				ID:          id,
+				Name:        val.Name,
+				Generation:  getGeneration(id),
+				PrimaryType: details_response.Types[0].TypeStructure.TypeName,
+				Stats: model.Stats{
+					HP:             details_response.Stats[0].BaseStat,
+					Attack:         details_response.Stats[1].BaseStat,
+					Defense:        details_response.Stats[2].BaseStat,
+					SpecialAttack:  details_response.Stats[3].BaseStat,
+					SpecialDefense: details_response.Stats[4].BaseStat,
+					Speed:          details_response.Stats[5].BaseStat,
+					BaseStatTotal:  getBST(details_response.Stats),
+				},
+			}
+			u.GeneralLogger.Println(pkmn)
+			p = append(p, pkmn)
+		} else {
+			pkmn := &model.Pokemon{
+				ID:            id,
+				Name:          val.Name,
+				Generation:    getGeneration(id),
+				PrimaryType:   details_response.Types[0].TypeStructure.TypeName,
+				SecondaryType: details_response.Types[1].TypeStructure.TypeName,
+				Stats: model.Stats{
+					HP:             details_response.Stats[0].BaseStat,
+					Attack:         details_response.Stats[1].BaseStat,
+					Defense:        details_response.Stats[2].BaseStat,
+					SpecialAttack:  details_response.Stats[3].BaseStat,
+					SpecialDefense: details_response.Stats[4].BaseStat,
+					Speed:          details_response.Stats[5].BaseStat,
+					BaseStatTotal:  getBST(details_response.Stats),
+				},
+			}
+			u.GeneralLogger.Println(pkmn)
+			p = append(p, pkmn)
+		}
 	}
 	return p, nil
+}
+
+func getBST(s []external.PokemonExternalStats) uint64 {
+	bst := 0
+	for _, v := range s {
+		bst += int(v.BaseStat)
+	}
+	return uint64(bst)
 }
 
 func getGeneration(id uint64) uint64 {
